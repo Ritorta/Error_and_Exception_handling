d48c340e97bea6a01fedbfe8254544ec4d5c8ab5
Ritorta <meddesu@yandex.ru>
2023-10-31 13:40:09 +0300

    format correct, add long phoneNumber

--- Work№3/Main.java
+++ Work№3/Main.java
@@ -35,12 +35,11 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the data in format: SurName, FirstName, Patronymic, Date of birth, Phone Number, You gender: M or F");
         String inputScanner = scanner.nextLine();
-
         //Check userData to 6
         try {
             // Split string
             String[] userData = inputScanner.trim().split(" ");
-
+            // Cheak to correct number of fields
             if (userData.length != 6) 
             {
                 throw new IllegalArgumentException
@@ -51,10 +50,10 @@ public class Main
             String FirstName = userData[1];
             String Patronymic = userData[2];
             String birthDate = userData[3];
-            String phoneNumber = userData[4];
+            // String phoneNumber = userData[4];
+            long phoneNumber = Long.parseLong(userData[4]);
             // String gender = userData[5];
             char gender = userData[5].charAt(0);
-
             //Check validate userData
             validateUserData(SurName, FirstName, Patronymic, birthDate, phoneNumber, gender);
             //Record userData
@@ -71,9 +70,13 @@ public class Main
             System.out.println("Error: Record or Read file");
             e.printStackTrace();
         }
+        finally
+        {
+            scanner.close();
+        }
     }
     // Validate UserData
-    private static void validateUserData(String SurName, String FirstName, String Patronymic, String birthDate, String phoneNumber, char gender)
+    private static void validateUserData(String SurName, String FirstName, String Patronymic, String birthDate, long phoneNumber, char gender)
     {
         // Validate SurName
         if(!SurName.matches("[a-zA-Z]+"))
@@ -104,17 +107,21 @@ public class Main
         {
             throw new IllegalArgumentException("Date values are incorrect");
         }
-        // Validate phoneNumber
-        if(!phoneNumber.matches("^\\+(\\d{1,3}(\\(\\d{1,3}\\))?)(?:[0-9] ?){6,14}[0-9]$"))
+        // Validate phoneNumber (String)
+        // if(!phoneNumber.matches("^\\+(\\d{1,3}(\\(\\d{1,3}\\))?)(?:[0-9] ?){6,14}[0-9]$"))
+        // {
+        //     throw new IllegalArgumentException("Incorrect Phone Number");
+        // }
+        // Validate phoneNumber (long)
+        if (phoneNumber < 70000000000L || phoneNumber > 79999999999L) 
         {
-            throw new IllegalArgumentException("Incorrect Phone Number");
+            throw new IllegalArgumentException("Incorrect Phone Number. The phone number should be within the range of +70000000000 to +79999999999.");
         }
         // Validate gender (String)
         // if(!gender.matches("(?i)^(m|f|male|female)$"))
         // {
         //     throw new IllegalArgumentException("Incorrect gender");
         // }
-
         // Validate gender (Char)
         if(!String.valueOf(gender).equalsIgnoreCase("f") && !String.valueOf(gender).equalsIgnoreCase("m"))
         {
@@ -122,13 +129,15 @@ public class Main
         }
     }
     // Record Data
-    private static void recUserData(String SurName, String FirstName, String Patronymic, String birthDate, String phoneNumber, char gender) throws IOException
+    private static void recUserData(String SurName, String FirstName, String Patronymic, String birthDate, long phoneNumber, char gender) throws IOException
     {
+        // Create fille
         String newFile = SurName + ".txt";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFile, true)))
         {
-            writer.write(SurName + FirstName + Patronymic + birthDate + phoneNumber + gender + "\n");
+            writer.write("SurName: " + SurName + ", FirstName: " + FirstName + ", Patronymic: " + Patronymic + 
+            ",  Date of birth: " + birthDate + ", Phone Number: " + phoneNumber + ", Gender: " + gender + ";" + "\n");
         }
     }
 }
